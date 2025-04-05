d4f7bd5a5d6d7a838697045cfdd73e933f6da98a
CP <125774593+chaezenp@users.noreply.github.com>
2025-04-04 22:29:04 -1000

    notes-1 finished in 55 minutes

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -23,6 +23,12 @@ const ListPage = async () => {
     },
   });
 
+  const notes = await prisma.note.findMany({
+    where: {
+      owner,
+    },
+  });
+
   return (
     <main>
       <Container id="list" fluid className="py-3">
@@ -33,7 +39,10 @@ const ListPage = async () => {
               <Row xs={1} md={2} lg={3} className="g-4">
                 {contacts.map((contact) => (
                   <Col key={contact.firstName + contact.lastName}>
-                    <ContactCard contact={contact} />
+                    <ContactCard
+                      contact={contact}
+                      notes={notes.filter(note => (note.contactId === contact.id))}
+                    />
                   </Col>
                 ))}
               </Row>
